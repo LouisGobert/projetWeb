c446a86b92c18fad2d3293ec345233fcaf9f1368
E180596 <l.gobert@student.helmo.be>
2018-12-08 13:39:56 +0100

    Initial commit

---
+++ inc/footer.inc.php
@@ -0,0 +1,14 @@
+<footer>
+  <nav id="image_conteneur">
+    <p><a href="https://www.facebook.com/" target="_blank"><img class="image_compte" src="images/reseau/facebook.png" alt="Facebook" title="Redirection vers notre page Facebook"></a></p>
+    <p><a href="https://twitter.com/?lang=fr" target="_blank"><img class="image_compte" src="images/reseau/twitter.png" alt="Twitter" title="Redirection sur notre page Twitter"></a></p>
+    <p><a href="https://www.instagram.com/?hl=fr" target="_blank"><img class="image_compte" src="images/reseau/instagram.png" alt="Instagram" title="Redirection sur notre page Instagram"></a></p>
+    <p><a href="https://www.youtube.com/?hl=FR" target="_blank"><img class="image_compte" src="images/reseau/youtube.png" alt="Youtube" title="Redirection sur notre chaine Youtube"></a></p>
+  </nav>
+  <nav id="footer_conteneur">
+    <p><a href="qui_somme_nous.html" title="Qui sommes-nous ?"><strong>Qui sommes-nous ? </strong></a>|</p>
+    <p><a href="mention.html" title="Voir nos mentions légales"><strong>Mentions légales </strong></a>|</p>
+    <p><a href="contact.html" title="Nous contacter"><strong>Nous contacter </strong></a></p>
+  </nav>
+  <p id="author">Par <strong>Gobert Louis</strong></p>
+</footer>
---
+++ inc/headerMembre.inc.php
@@ -0,0 +1,29 @@
+<header class="main">
+			
+	<a href="index.php">
+		<img src="images/reseau/logoBlueCow-small.png" alt="BlueCow®">
+	</a>
+	
+  <p id="title"><span>BlueCow®</span> Boissons révolutionnaires à base d'<b>oxyde</b> de <b>bi-hydrogène.</b></p>
+
+	<form>
+		<input type="text" name="recherche" placeholder="Rechercher...">
+	</form>
+  
+  
+  <nav class="navigation_bar">
+     
+    <a href="evenement.html" title="Tous les événements">Evénements <i class="far fa-calendar-alt"></i></a>
+    <a href="lieux.html" title="Tous les lieux">Lieux <i class="fas fa-map-marker-alt"></i></a>
+    
+
+    <div id="float_right">
+      <a href="inscription.php">Inscription <i class="fas fa-user-plus"></i></a>   
+      <a href="connexion.html">Connexion <i class="fas fa-sign-in-alt"></i></a>   
+    </div>
+  </nav>
+  <div class="inutile">bb</div>
+
+  <h1 class="position">Accueil</h1>
+
+</header>
